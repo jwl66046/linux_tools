64a753cfcd75b4cc735beaf6d6ab0c86c9c1ffe5
John Lawrence <kungfumastah@users.noreply.github.com>
2017-04-08 14:28:49 -0500

    Update disable_dnsmasq.sh

--- disable_dnsmasq.sh
+++ disable_dnsmasq.sh
@@ -7,6 +7,13 @@ sudo service networking restart
 
 sudo killall dnsmasq
 
+## add to /etc/dhcp/dhclient.conf:
+supersede domain-name-servers 8.8.8.8, 8.8.4.4;
+
+
+
+
+
 ## If you want to override or append to the name servers suggested by the DHCP server, 
 ## you can configure this in /etc/dhcp/dhclient.conf, eg:
 
