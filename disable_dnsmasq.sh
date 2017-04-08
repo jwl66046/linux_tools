f9bb5f8766fdca524cc473fe1dc452dddf9ccaeb
John Lawrence <kungfumastah@users.noreply.github.com>
2017-04-08 14:04:33 -0500

    Update disable_dnsmasq.sh

--- disable_dnsmasq.sh
+++ disable_dnsmasq.sh
@@ -7,3 +7,8 @@ sudo service networking restart
 
 sudo killall dnsmasq
 
+## If you want to override or append to the name servers suggested by the DHCP server, 
+## you can configure this in /etc/dhcp/dhclient.conf, eg:
+
+## interface "eth0" {
+##    prepend domain-name-servers 10.0.0.2;
